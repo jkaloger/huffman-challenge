964f33508eb88756c28d788c839c568459f66ea9
Jack Kaloger <jack.kaloger@inlight.com.au>
2023-12-05 23:59:23 +1100

    refactor, add tests

--- js/index.test.ts
+++ js/index.test.ts
@@ -2,6 +2,18 @@ import { test, expect } from "bun:test";
 
 import { encode, decode } from "./index";
 
+const raw = "this text is totally rad, dude";
+const encoded =
+  "111 01011 1000 1011 110 111 0111 0001 111 110 1000 1011 110 111 10100 111 0110 1001 1001 0100 110 10101 0110 001 01010 110 001 0000 001";
+
 test("encodes correctly", () => {
-  expect(encode("this text is totally rad, dude")).toBe("111 01011 1000 1011 110 111 0111 0001 111 110 1000 1011 110 111 10100 111 0110 1001 1001 0100 110 10101 0110 001 01010 110 001 0000 001");
+  expect(encode(raw)).toBe(encoded);
+});
+
+test("decodes correctly", () => {
+  expect(decode(encoded)).toBe(raw);
+});
+
+test("decode/encodes correctly", () => {
+  expect(decode(encode(raw))).toBe(raw);
 });
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,15 +1,12 @@
 fn main() {
-    println!(
-        "Encoding 'Hello, world!': {}",
-        encode(String::from("Hello, world!"))
-    );
+    println!("Encoding 'Hello, world!': {}", encode("Hello, world!"));
 }
 
-pub fn encode(input: String) -> String {
+pub fn encode(input: &str) -> &str {
     return input;
 }
 
-pub fn decode(input: String) -> String {
+pub fn decode(input: &str) -> &str {
     return input;
 }
 
@@ -17,8 +14,21 @@ pub fn decode(input: String) -> String {
 mod tests {
     use super::*;
 
+    const RAW: &str = "this text is totally rad, dude";
+    const ENCODED: &str = "111 01011 1000 1011 110 111 0111 0001 111 110 1000 1011 110 111 10100 111 0110 1001 1001 0100 110 10101 0110 001 01010 110 001 0000 001 0111";
+
     #[test]
     fn test_encode() {
-        assert_eq!(encode(String::from("this text is totally rad, dude")), String::from("111 01011 1000 1011 110 111 0111 0001 111 110 1000 1011 110 111 10100 111 0110 1001 1001 0100 110 10101 0110 001 01010 110 001 0000 001 0111"));
+        assert_eq!(encode(RAW), ENCODED);
+    }
+
+    #[test]
+    fn test_decode() {
+        assert_eq!(decode(ENCODED), RAW);
+    }
+
+    #[test]
+    fn test_both() {
+        assert_eq!(decode(encode(RAW)), RAW);
     }
 }
